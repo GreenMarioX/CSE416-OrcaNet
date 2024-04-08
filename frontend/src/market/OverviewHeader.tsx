b58688e4b36809e71e8e4b985fc6dba7270ad4ed
Billy Li <billy.li.1@stonybrook.edu>
2024-04-08 04:51:37 -0400

    Added functionality to Add Jobs button

--- frontend/src/market/OverviewHeader.tsx
+++ frontend/src/market/OverviewHeader.tsx
@@ -2,7 +2,18 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Filter, PlusIcon } from "lucide-react";
 import { useState } from "react";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+import { DataTable } from "./DataTable";
+import fakeSeeds from "./fakeSeeds";
+import { Seed2, columns2 } from "./columns";
 const OverviewHeader = (props: {
   setFilter: React.Dispatch<React.SetStateAction<string>>;
   setStatusFilter: React.Dispatch<React.SetStateAction<string>>;
@@ -69,40 +80,67 @@ const FilterInput = (props: {
   );
 };
 
+const fakeData: Seed2[] = [
+  { name: "Alice", price: "30 USD", rate: "30 Mb/s"},
+  { name: "Bob", price: "35 USD", rate: "50 Mb/s"}
+]
+const DialogClose = DialogPrimitive.Close
 const AddJob = (props: { addJob: (hash: string) => void }) => {
-  const [active, setActive] = useState(false);
   const [buffer, setBuffer] = useState("");
+  const [validHash, setValidHash] = useState(0);
+  //const [selectedPeer, setSelectedPeer] = useState(false)
+  const hash = "f26811862f6ba50908850f61ad73fb5d8d86754cd2acb9ef";
+
+  const resetAddJob = () => {
+    setBuffer("");
+    setValidHash(0);
+  }
+
+  const handleSearchHash = () => {
+    if (buffer === hash) {
+      setValidHash(1);
+      console.log("Hello");
+    }
+    else{
+      setValidHash(-1);
+    }
+  };
+
+  const handleAddJob = () => {
+
+    props.addJob(buffer);
+  }
+
   return (
     <div>
-      {active ? (
-        <div className="relative min-w-28">
+      <Dialog>
+        <DialogTrigger>
+          <Button onClick={resetAddJob} className="ml-20">Add Job</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Add Job</DialogTitle>
           <Input
             type="text"
             value={buffer}
             onChange={(e) => setBuffer(e.target.value)}
             onKeyUp={(e) => {
               if (e.key === "Enter") {
-                props.addJob(buffer);
-                setBuffer("");
-                setActive(false);
+                handleSearchHash();
               }
             }}
-            placeholder="Filter..."
+            placeholder="File hash..."
           />
-          <PlusIcon
-            className="absolute right-2 top-2"
-            onClick={(e) => {
-              props.addJob(buffer);
-              setBuffer("");
-              setActive(false);
-            }}
-          />
-        </div>
-      ) : (
-        <Button className="ml-20" onClick={(e) => setActive(true)}>
-          Add Job
-        </Button>
-      )}
+          {validHash !== 1 && <Button onClick={handleSearchHash}>Search Hash</Button>}
+          {validHash === -1 && <p>No files match this hash</p>}
+          {validHash === 1 && (
+          <>
+           <DataTable data={fakeData} columns={columns2}></DataTable>
+            <DialogClose>
+              <Button onClick={handleAddJob}>Select Peer</Button>
+            </DialogClose>
+          </>)}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- frontend/src/market/columns.tsx
+++ frontend/src/market/columns.tsx
@@ -97,3 +97,75 @@ export const columns: ColumnDef<Seed>[] = [
     ),
   },
 ];
+
+
+export type Seed2 = {
+  name: string;
+  price: string;
+  rate: string;
+};
+
+export const columns2: ColumnDef<Seed2>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "rate",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Transfer Rate
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  }
+]
